1bfcfa007560ae0bceefaf1e293569c7f26675ed
gluaxspeed <jonathan.t.pavlik@gmail.com>
2018-11-19 15:03:24 -0500

    tests for auth written

--- auth_test.go
+++ auth_test.go
@@ -5,16 +5,28 @@ import (
 	"encoding/json"
 	"net/http"
 	"net/http/httptest"
+	"os"
 	"testing"
 
 	"backend/api"
 	"backend/models"
 
 	assert "github.com/stretchr/testify/assert"
+
+	"github.com/stevens-tyr/tyr-gin"
+	bson "gopkg.in/mgo.v2/bson"
 )
 
-func performRequest(r http.Handler, method, path string, body []byte) *httptest.ResponseRecorder {
+func cleanUserDB(email string) {
+	db, _ := tyrgin.GetMongoDB(os.Getenv("DB_NAME"))
+	assignCol, _ := tyrgin.GetMongoCollectionCreate("users", db)
+
+	assignCol.Remove(bson.M{"email": email})
+}
+
+func performRequest(r http.Handler, method, path string, body []byte, token string) *httptest.ResponseRecorder {
 	var req *http.Request
+
 	if method == "GET" {
 		req, _ = http.NewRequest(method, path, nil)
 	} else {
@@ -24,6 +36,12 @@ func performRequest(r http.Handler, method, path string, body []byte) *httptest.
 		req.Header.Set("Accept", "application/json")
 		req.Header.Set("Content-Type", "application/json; charset=utf-8")
 	}
+
+	if token != "" {
+		bearer := "Bearer " + token
+		req.Header.Add("Authorization", bearer)
+	}
+
 	resp := httptest.NewRecorder()
 	r.ServeHTTP(resp, req)
 	return resp
@@ -31,14 +49,96 @@ func performRequest(r http.Handler, method, path string, body []byte) *httptest.
 
 func TestInvalidLogin(t *testing.T) {
 	server := api.SetUp()
-	server.Run(":5555")
 
+	cleanUserDB("test@test.com")
 	loginData := models.Login{
 		Email:    "test@test.com",
 		Password: "tester123",
 	}
 	jsb, _ := json.Marshal(loginData)
 
-	resp := performRequest(server, "POST", "/api/v1/auth/login", jsb)
+	resp := performRequest(server, "POST", "/api/v1/auth/login", jsb, "")
 	assert.Equal(t, 401, resp.Code)
+}
+
+func TestInvalidRegister(t *testing.T) {
+	server := api.SetUp()
+	cleanUserDB("test@test.com")
+
+	registerData := models.RegisterForm{
+		Email:                "test@test.com",
+		Password:             "tester123",
+		PasswordConfirmation: "tester1234",
+		First:                "Bob",
+		Last:                 "Bobert",
+	}
+	jsb, _ := json.Marshal(registerData)
+
+	resp := performRequest(server, "POST", "/api/v1/auth/register", jsb, "")
+	assert.Equal(t, 400, resp.Code)
+}
+
+func TestTakenRegister(t *testing.T) {
+	server := api.SetUp()
+	cleanUserDB("test2@test.com")
+
+	registerData := models.RegisterForm{
+		Email:                "test2@test.com",
+		Password:             "tester123",
+		PasswordConfirmation: "tester123",
+		First:                "Bob",
+		Last:                 "Bobert",
+	}
+	jsb, _ := json.Marshal(registerData)
+
+	resp := performRequest(server, "POST", "/api/v1/auth/register", jsb, "")
+	assert.Equal(t, 200, resp.Code)
+
+	registerData = models.RegisterForm{
+		Email:                "test2@test.com",
+		Password:             "tester123",
+		PasswordConfirmation: "tester123",
+		First:                "Bob",
+		Last:                 "Bobert",
+	}
+	jsb, _ = json.Marshal(registerData)
+
+	resp = performRequest(server, "POST", "/api/v1/auth/register", jsb, "")
+	assert.Equal(t, 400, resp.Code)
+
+	cleanUserDB("test2@test.com")
+}
+
+func TestValidRegisterAndLogin(t *testing.T) {
+	server := api.SetUp()
+	cleanUserDB("test@test.com")
+
+	registerData := models.RegisterForm{
+		Email:                "test@test.com",
+		Password:             "tester123",
+		PasswordConfirmation: "tester123",
+		First:                "Bob",
+		Last:                 "Bobert",
+	}
+	jsb, _ := json.Marshal(registerData)
+
+	resp := performRequest(server, "POST", "/api/v1/auth/register", jsb, "")
+	assert.Equal(t, 200, resp.Code)
+
+	loginData := models.Login{
+		Email:    "test@test.com",
+		Password: "tester123",
+	}
+	jsb, _ = json.Marshal(loginData)
+
+	resp = performRequest(server, "POST", "/api/v1/auth/login", jsb, "")
+	assert.Equal(t, 200, resp.Code)
+
+	var body map[string]interface{}
+	json.NewDecoder(resp.Body).Decode(&body)
+
+	resp = performRequest(server, "GET", "/api/v1/auth/logged_in", nil, body["token"].(string))
+	assert.Equal(t, 200, resp.Code)
+
+	cleanUserDB("test@test.com")
 }
