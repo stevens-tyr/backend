a0cd3762169700bcb2558c0c64a1b5b3c3f1d9d6
Robert Herley <rherley@stevens.edu>
2019-04-20 18:07:02 -0400

    fix patch course fields

--- api/cms/update_course.cms.go
+++ api/cms/update_course.cms.go
@@ -21,20 +21,20 @@ func UpdateCourse(c *gin.Context) {
 		c.Set("error", errors.ErrorInvlaidJSON)
 		return
 	}
-	
-	if val, ok := up["department"]; ok { 
+
+	if val, ok := up["department"]; ok {
 		course.Department = val.(string)
 	}
-	if val, ok := up["department"]; ok {
+	if val, ok := up["longName"]; ok {
 		course.LongName = val.(string)
 	}
-	if val, ok := up["department"]; ok {
+	if val, ok := up["number"]; ok {
 		course.Number = val.(int)
 	}
-	if val, ok := up["department"]; ok {
+	if val, ok := up["section"]; ok {
 		course.Section = val.(string)
 	}
-	if val, ok := up["department"]; ok {
+	if val, ok := up["semester"]; ok {
 		course.Semester = val.(string)
 	}
 
@@ -43,7 +43,7 @@ func UpdateCourse(c *gin.Context) {
 		c.Set("error", err)
 		return
 	}
-	
+
 	c.JSON(200, gin.H{
 		"message": "Course Updated.",
 	})
